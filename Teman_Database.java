b6d128da5b41dd9d66b69d17f534f9907a6b1ebf
rafi-dev22 <muhammad_prayata_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:27:02 +0700

    Add files via upload

---
+++ Teman_Database.java
@@ -0,0 +1,43 @@
+public class Teman_Database {
+
+public static void main(String[] args) {
+    //nama teman
+    String namateman1 = "Erlangga Dwi F.";
+    String namateman2 = "Atha Khoirun Ayyasi";
+    String namateman3 = "Farhan haidas Ghasani";
+    String namateman4 = "Intazar Lintang Pramanta";
+    String namateman5 = "Sakti Mahaya zaman";
+    
+    //umur teman
+    int umurteman1 = 15;
+    int umurteman2 = 15;
+    int umurteman3 = 15;
+    int umurteman4 = 16;
+    int umurteman5 = 15;
+    
+    //cita cita teman
+    String citacitateman1 = "Orang sukses";
+    String citacitateman2 = "Pengusaha sukses";
+    String citacitateman3 = "Pengusaha sukses";
+    String citacitateman4 = "IT Developer";
+    String citacitateman5 = "Sukses";
+
+    System.out.println("Erlangga Dwi F.");
+    System.out.println("Atha Khoirun Ayyasi");
+    System.out.println("Farhan haidas Ghasani");
+    System.out.println("Intazar Lintang Pramanta");
+    System.out.println("Sakti Mahaya zaman");
+
+    System.out.println("15");
+    System.out.println("15");
+    System.out.println("15");
+    System.out.println("16");
+    System.out.println("15");
+
+    System.out.println("Orang sukses");
+    System.out.println("Pengusaha sukses");
+    System.out.println("Pengusaha sukses");
+    System.out.println("IT Developer");
+    System.out.println("sukses");
+}
+}
