73cecff56d6c7aa894c4c9137979059dfa5cab6c
rafi-dev22 <muhammad_prayata_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:22:06 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,6 +1,15 @@
+
+
 public class hello{
     public static void main(String[] args) {
+        //main method
         System.out.println("x:Muhammad gavrila rafif prayata;");
+        System.out.println("x:SMK Telkom Malang;");
+        System.out.println("x:SMP BRAWIJAYA SMART SCHOOL MALANG;");
+        System.out.println("x:HELMI KURNIAWAN;");
+        System.out.println("x:DWI KARTIKA WARDHANI;");
+        System.out.println("x:Pengusaha;");
+        System.out.println("x:Membaca Buku;");
     }
 }
 
